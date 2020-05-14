b54b33a4728fafa12cebaafb2bc0253ff6d3cfed
lineho <myksh0316@gmail.com>
2020-05-14 12:48:52 +0900

    Form_main setting
    changed Form name

--- sunho/SPDRC_PROGRAM/SPDRC_PROGRAM/Program.cs
+++ sunho/SPDRC_PROGRAM/SPDRC_PROGRAM/Program.cs
@@ -16,7 +16,7 @@ namespace SPDRC_PROGRAM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_main());
         }
     }
 }
