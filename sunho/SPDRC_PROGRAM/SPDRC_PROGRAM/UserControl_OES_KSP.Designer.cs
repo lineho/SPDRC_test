d564de8b62692101fb88d19f00c0ecb11e106355
Joon <32371480+loveey0928@users.noreply.github.com>
2020-06-30 00:19:33 +0900

    testteesttest

--- sunho/SPDRC_PROGRAM/SPDRC_PROGRAM/UserControl_OES_KSP.Designer.cs
+++ sunho/SPDRC_PROGRAM/SPDRC_PROGRAM/UserControl_OES_KSP.Designer.cs
@@ -44,6 +44,8 @@
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.dgv_1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.lineRatioGraph)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -176,17 +178,37 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(912, 111);
+            this.button1.Location = new System.Drawing.Point(836, 108);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 9;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(846, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(883, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // UserControl_OES_KSP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.btn_drawGraph);
@@ -222,5 +244,7 @@
         private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.DataGridView dgv_1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
